206aa51a0eec2dffd72b36f2bb1e00d2c6ea714d
Your Name <you@example.com>
2021-04-18 22:19:31 +0700

    first commit

---
+++ js/slides.js
@@ -0,0 +1,10 @@
+const slide = document.getElementById('glide')
+
+new Glide(slide, {
+
+    type:'carousel',
+    startAt: 0,
+    preView:1,
+    animationDuration :800,
+    animationTimingFunc: "linear",
+}).mount();
